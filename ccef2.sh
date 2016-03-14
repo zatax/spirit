d13a146efe6fa12c9b1077ddef88478ff7c17eb4
zatax <zataxx@gmail.com>
2016-03-14 11:53:48 +0100

    ordre

---
+++ ccef2.sh
@@ -0,0 +1,175 @@
+#!/bin/bash 
+
+
+#---------------------------------------#
+#					#
+#	    CREATION COMPTE		#
+#	      ENGINE FRAME		#
+#		  TOTAL			#
+#					#
+#---------------------------------------#
+
+#########################################
+#					#
+#-------|    variables  | --------------#
+#					#
+#					#
+#       Serveur : $opt			# 
+#	NOM	: $NOM			#
+#       PRENOM	: $PRENOM		#
+#       HOME	: $HOME			#
+#       INC	: $INC			#
+#	GID	: $gid			#
+#	IGG	: $IGG			#
+#	UID	: $UID2			#
+#					#
+#---------------------------------------#
+#########################################
+
+#########################################
+#					#
+#--------|    Fonction   | -------------#
+#					#
+#					#
+#       ROOT	: Check ROOT		#
+#	MENU1	: Choix serveur 	#
+#       name	: NOM et PRENOM		#
+#       home	: HOME 			#
+#       INC	: INC			#
+#       GID     : GID			#
+#	IGG	: IGG			#
+#					#
+#---------------------------------------#
+#########################################
+
+
+
+
+#######################
+#-----check Root------#
+#######################
+
+root () {
+        if [ "$(id -u)" != "0" ]; then
+                echo "Vous devez être root pour executer ce script"
+                exit 1
+        fi
+        }
+
+
+
+
+####################################
+#------ choix serv TXT -------------
+####################################
+
+
+menu1 () {
+        echo "Choix du serveur"
+        serv=$(cat serv.txt)
+        tableau=( ${serv//./ } ) 
+
+        eval set ${tableau[@]}
+        select opt in "$@" 
+        do  
+                echo $opt  
+                        if [ "$opt"="quit" ]; then
+                        break
+                        fi  
+        done 
+        }   
+
+
+###################################
+#------- NOM - PRENOM-------------#
+###################################
+
+name () {
+        echo
+        NOM=""
+        PRENOM=""
+        while [[ -z $NOM  ]] || [[ -z $PRENOM ]]; do
+                read -p "NOM : " NOM
+                read -p "PRENOM : " PRENOM
+        done
+        echo $NOM $PRENOM
+        }
+
+#################################
+#------ creation HOME -----------#
+#################################
+
+
+
+home () {
+        echo "choix de la home"
+        home=""
+        while [[ -z $home ]]; do
+                read -p "répertoire HOME : " home
+        done
+        echo $home
+
+        }
+
+#################################
+#------ creation INC -----------#
+#################################
+
+
+
+INC () {
+        echo "INC compte Total"
+        INC=""
+        while [[ -z $INC ]]; do
+                read -p "Numéro INC: " INC
+        done
+        echo $INC
+
+        }
+
+
+
+#################################
+#------ creation GID -----------#
+#################################
+
+
+gid () {
+        echo "Identifiant unique de compte (GID)"
+        gid=""
+        while [[ -z $gid ]]; do
+                read -p "GID : " gid 
+        done
+        echo $gid
+
+        }   
+
+
+#############################################
+#---------- création IGG - UID  ------------#
+#                                           #
+#       UID = IGG du compte moins les       # 
+#             les deux premièrs caractères  #
+#                                           #
+#############################################
+
+
+IGG () {
+        IGG=""
+        while [[ -z $IGG ]]; do
+                read -p "IGG Total : " IGG
+        done
+        echo $IGG
+
+        UID2=${IGG:2} 
+
+        }
+
+
+TIGG () {
+	
+	gid 
+
+}
+
+TIGG
